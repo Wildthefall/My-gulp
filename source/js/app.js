581518a492c9e506ac9af13fa3550904d7c66202
Stas Melnickov <mielnikov.stas@mail.ru>
2017-07-07 21:42:35 +0500

    added parallax

--- source/js/app.js
+++ source/js/app.js
@@ -15,3 +15,24 @@ window.onload = function() {
         }
     })
 };
+
+$(document).ready(function() {
+
+    var layer = $('.parallax').find('.parallax__layer');
+
+    $(window).on('mousemove', function(event) {
+        var mouseX = event.pageX,
+            mouseY = event.pageY,
+            width = (window.innerWidth / 2) - mouseX,
+            height = (window.innerHeight / 2) - mouseY;
+
+        layer.map(function(key, value) {
+            var widthPosition = width * (key / 150),
+                heightPosition = height * (key / 150);
+
+            $(value).css({
+                'transform': 'translate3d(' + widthPosition + 'px, ' + heightPosition + 'px, 0)'
+            })
+        })
+    })
+});
